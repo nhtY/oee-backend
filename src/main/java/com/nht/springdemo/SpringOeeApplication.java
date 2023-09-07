e5ebdf1ef6aa8254050d67174c9ac42f6cc65332
nthY <n_hat.yalcin@outlook.com>
2023-09-07 11:57:53 +0300

    init

---
+++ src/main/java/com/nht/springdemo/SpringOeeApplication.java
@@ -0,0 +1,13 @@
+package com.nht.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nht/springdemo/SpringOeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nht.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
