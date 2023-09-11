968f731ae1ff9c14194845e712a9b478c28027ae
nthY <n_hat.yalcin@outlook.com>
2023-09-11 13:25:25 +0300

    create OeePerformanceController

---
+++ src/main/java/com/nht/springdemo/controller/OeePerformanceController.java
@@ -0,0 +1,31 @@
+package com.nht.springdemo.controller;
+
+import com.nht.springdemo.services.OeePerformanceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/oee")
+public class OeePerformanceController {
+
+    private final OeePerformanceService oeePerformanceService;
+
+    @GetMapping("/current")
+    public ResponseEntity getCurrentOeeData() {
+        return ResponseEntity.ok(oeePerformanceService.getCurrentOeeData());
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity getAll() {
+        return ResponseEntity.ok(oeePerformanceService.getAllOeeData());
+    }
+
+    @GetMapping("/last-20")
+    public ResponseEntity getlLastTwenty() {
+        return ResponseEntity.ok(oeePerformanceService.getLastTwentyOeeData());
+    }
+}
